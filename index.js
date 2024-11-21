0fe2c98a15356e30f45bddfc40cf3f0fe3103dda
abmgryan <ryandougherty@atlanticbay.com>
2024-11-21 12:43:50 -0500

    Update index.js
    Updated for API Redirect Test

--- index.js
+++ index.js
@@ -1,46 +1,93 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const bodyParser = require('body-parser');
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 
+const urls = [{original_url: 'http://localhost:3000/', short_url: 1}, {original_url: 'https://www.google.com/', short_url: 2}, {original_url: 'www.yahoo.com', short_url: 3}];
 
-app.get('/api/whoami', function (req, res) {
+
+app.post('/api/shorturl', function(req, res) {
   
-  let reqAddress = req.ip;
-  let reqLanguage = req.headers['accept-language'];
-  let reqSoftware = req.headers['sec-ch-ua'];
+
+  //let newURL = bodyParser.raw(req.body)
+
+  console.log('POST')
+  console.log('req.body: ' + req.body)
+  console.log('JSON.stringify(req.body): ' + JSON.stringify(req.body))
+  console.log('POST')
+  console.log('req.body.url: ' + req.body.url)
+  //console.log('JSON.raw(req.body): ' + JSON.raw(req.body))
+  console.log('POST')
+  console.log('this is being added to array: ' + '{ original_url: ' + req.body.url + ', short_url: ' + (urls.length + 1) + '}')
   
-  
-  
-  res.json({ ipaddress: reqAddress, language: reqLanguage, software: reqSoftware });
+  urls.push({ original_url: req.body.url, short_url: urls.length + 1 })  
+
+  console.log('POST')
+  console.log(urls)
+
+  res.json({ original_url: req.body.url, short_url: urls.length});
 });
 
 
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+
+
+
+app.get('/api/shorturl/:url', function(req, res) {
+  
+  console.log('Request: ' + JSON.stringify(req.params));
+  console.log('Requested url: ' + req.params.url);
+  console.log('url location: ' + urls.findIndex((test) => {
+    console.log('');
+    console.log('test #: ' + JSON.stringify(test));
+    console.log('test.short_url: ' + test.short_url);
+    console.log('req.params.url: ' + req.params.url);
+    console.log(test.short_url === req.params.url);
+    console.log(Number(test.short_url) === Number(req.params.url));
+    Number(test.short_url) === Number(req.params.url);
+  }));
+  console.log('index of: ' + urls.indexOf(req.params.url));
+
+
+  let arrayLocation = urls.findIndex((test) => {
+    console.log(Number(test.short_url) === Number(req.params.url));
+    return Number(test.short_url) === Number(req.params.url);
+  })
+
+  console.log('arraylocation: ' + arrayLocation);
+
+  //res.json({result: urls.findIndex((test) => {test == req.params})})
+  //res.send('url location: ' + urls.findIndex((test) => {return Number(test.short_url) === Number(req.params.url)}));
+  //res.send(urls[arrayLocation].original_url);
+  res.redirect(urls[arrayLocation].original_url);
+});
+
+
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
