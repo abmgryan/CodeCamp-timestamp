0420b675bc9f833d866c24c760f4f908a4059ab3
abmgryan <ryandougherty@atlanticbay.com>
2024-11-22 12:10:47 -0500

    Update index.js
    Updated for 'workout' certification

--- index.js
+++ index.js
@@ -1,53 +1,117 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
 
-const urls = [{original_url: 'http://localhost:3000/', short_url: 1}, {original_url: 'https://www.google.com/', short_url: 2}, {original_url: 'www.yahoo.com', short_url: 3}];
+
+const users = [{username: 'Ryan', _id: 1}];
+const workouts = [{_id: '1',description: 'run', duration: '30', date: '11/22/2024'}]
 
 
-app.post('/api/shorturl', function(req, res) {
+
+app.post('/api/users', (req, res, next) =>{
+  const username = req.body.username;
+
+  users.push({username: username, _id: users.length});
+
+  res.json({username: username, _id: users.length});
+});
+
+
+app.get('/api/users', (req, res, next) =>{
+
+
+  res.json(users);
+});
+
+
+
+app.post('/api/users/:_id/exercises', (req, res, next) =>{
   
-
-  //let newURL = bodyParser.raw(req.body)
-
-  console.log('POST')
-  console.log('req.body: ' + req.body)
-  console.log('JSON.stringify(req.body): ' + JSON.stringify(req.body))
-  console.log('POST')
-  console.log('req.body.url: ' + req.body.url)
-  //console.log('JSON.raw(req.body): ' + JSON.raw(req.body))
-  console.log('POST')
-  console.log('this is being added to array: ' + '{ original_url: ' + req.body.url + ', short_url: ' + (urls.length + 1) + '}')
+  console.log('req.params._id: ' + req.params._id)
+  console.log('users[req.params._id]: ' + users[Number(req.params._id)])
   
-  urls.push({ original_url: req.body.url, short_url: urls.length + 1 })  
+  const usernameID = req.params._id;
+  const username = users[req.params._id-1].username;
 
-  console.log('POST')
-  console.log(urls)
+  //check if date is valid
+  let workoutDate = req.body.date;
+  try{
+    workoutDate = new Date(req.body.date);
+  } catch(e) {
+    console.log('Invalid Date Format');
+    workoutDate = new Date();
+  }
+  if(!workoutDate){ workoutDate = new Date()};
 
-  res.json({ original_url: req.body.url, short_url: urls.length});
+  const postedDate = new Date(req.body.date);
+
+  workouts.push({_id: usernameID, description: req.body.description, duration: req.body.duration, date: req.body.date});
+
+  res.json({_id: usernameID, username: username, description: req.body.description, duration: req.body.duration, date: postedDate.toDateString()});
+});
+
+// app.get('/api/users/:_id/logs', (req, res, next) =>{
+//   const username = users[req.params._id-1].username;
+
+
+//   workoutArray = [];
+//   workoutCount = 0;
+//   for (let record in workouts){
+//     if(workouts[record]._id == req.params._id){
+//       workoutArray.push({description: workouts[record].description, duration: Number(workouts[record].duration), date: workouts[record].date});
+//       workoutCount ++;
+//     }
+//   }
+
+
+//   res.json({_id: req.params._id, username: username, count: workoutCount, logs: workoutArray})
+// });
+
+
+//?[from][&to][&limit]
+app.get('/api/users/:_id/logs', (req, res, next) =>{
+  const username = users[req.params._id-1].username;
+
+  console.log('from: ' + req.query.from);
+  console.log('to: ' + req.query.to);
+  console.log('limit: ' + req.query.limit);
+
+  workoutArray = [];
+  workoutCount = 0;
+  breakException = false;
+  for (let record in workouts){
+    if(!breakException){
+      if(workouts[record]._id == req.params._id){
+        if((!req.query.from || workouts[record].date >= req.query.from) && (!req.query.to || workouts[record].date <= req.query.to)){
+          
+          //if(workoutCount != undefined && workoutCount != null) {if(workoutCount == req.query.limit){breakException = true;};};
+          
+
+          if(workoutCount == undefined || workoutCount == null || workoutCount != req.query.limit){
+          workoutArray.push({description: workouts[record].description, duration: Number(workouts[record].duration), date: new Date(workouts[record].date).toDateString()});
+          workoutCount ++;
+          } else {
+            breakException = true;
+          }
+        }
+      }
+    }
+  }
+
+
+  res.json({_id: req.params._id, username: username, count: workoutCount, logs: workoutArray})
 });
 
 
@@ -57,37 +121,8 @@ app.post('/api/shorturl', function(req, res) {
 
 
 
-app.get('/api/shorturl/:url', function(req, res) {
-  
-  console.log('Request: ' + JSON.stringify(req.params));
-  console.log('Requested url: ' + req.params.url);
-  console.log('url location: ' + urls.findIndex((test) => {
-    console.log('');
-    console.log('test #: ' + JSON.stringify(test));
-    console.log('test.short_url: ' + test.short_url);
-    console.log('req.params.url: ' + req.params.url);
-    console.log(test.short_url === req.params.url);
-    console.log(Number(test.short_url) === Number(req.params.url));
-    Number(test.short_url) === Number(req.params.url);
-  }));
-  console.log('index of: ' + urls.indexOf(req.params.url));
 
 
-  let arrayLocation = urls.findIndex((test) => {
-    console.log(Number(test.short_url) === Number(req.params.url));
-    return Number(test.short_url) === Number(req.params.url);
-  })
-
-  console.log('arraylocation: ' + arrayLocation);
-
-  //res.json({result: urls.findIndex((test) => {test == req.params})})
-  //res.send('url location: ' + urls.findIndex((test) => {return Number(test.short_url) === Number(req.params.url)}));
-  //res.send(urls[arrayLocation].original_url);
-  res.redirect(urls[arrayLocation].original_url);
-});
-
-
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
